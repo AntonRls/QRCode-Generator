f29aa08e21f53e3ca7135f557fbf60fdbaa78e78
AntonRls <74132592+AntonRls@users.noreply.github.com>
2021-12-16 21:50:23 +0300

    Project
    Project

---
+++ QRCode generator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCode_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QRCode qr = new QRCode(1000, true, Brushes.White);
+            qr.GenerateCode("привет");
+            qr.SaveCode("res.png");
+        }
+     }
+}
